4624498942e36914eb3a5412b502477c74c5b2bb
Sassa Kitka <sassa.kitka@gmail.com>
2016-09-30 09:58:49 -0700

    added index, destroy, create methods

--- app/controllers/beetle_controller.rb
+++ app/controllers/beetle_controller.rb
@@ -1,13 +1,22 @@
 class BeetleController < ApplicationController
   def index
+    @beetles = Beetle.all
   end
 
   def new
-
+    @beetle = Beetle.new
   end
 
   def create
-    @beetle = Beetle.new
+    @params = params
+    @title = params[:beetle][:name]
+    @description = params[:beetle][:description]
+
+    @beetle = Beetle.new(name: @title, description: @description)
+    # redirect_to root_path
+
+
+    # @beetle = Beetle.new
     if @beetle.save
       redirect_to root_path
     else
@@ -19,7 +28,11 @@ class BeetleController < ApplicationController
   def show
   end
 
-  def destroy
+  def destroy    
+    @beetle = Beetle.find_by(params[:id])
+    @beetle.destroy
+    redirect_to root_path
+
   end
 
   def edit
